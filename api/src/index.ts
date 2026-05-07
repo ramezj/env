35adc7333020cacdd177aba898d94b0cf36e1194
ramezj <ramezjoseph8@gmail.com>
2026-05-07 19:11:57 +0300

    better auth drizzle init

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,19 @@
+import "dotenv/config";
 import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./auth";
+import cors from "cors";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your frontend's origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  }),
+);
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
